8e14127784c3dc3518c0a5881f757335e8527118
Hund335 <koenigderpaulberger@gmail.com>
2024-12-18 10:56:39 +0200

    Update new locations.lua
    updated the locations to be in a new table and format

--- new locations.lua
+++ new locations.lua
@@ -1,48 +1,56 @@
 -- // // // LOCATIONS // // // --
-name = "Moosewood" = {Position = Vector3.new(470.000000, 150.935791, 260.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "The Depths" = {Position = Vector3.new(853.240295, -740.365906, 1335.115723), Rotation = Vector3.new(180.000000, 55.258999, 180.000000)}
-name = "Ancient Isles" = {Position = Vector3.new(5805.021484, 135.301498, 405.922119), Rotation = Vector3.new(0.000000, -42.668999, 0.000000)}
-name = "Forsaken Shores" = {Position = Vector3.new(-2675.331055, 164.795013, 1758.057129), Rotation = Vector3.new(0.000000, 75.242996, 0.000000)}
-name = "Enchant" = {Position = Vector3.new(1310.264404, -805.292236, -99.972527), Rotation = Vector3.new(180.000000, -1.252000, 180.000000)}
-name = "Terapin" = {Position = Vector3.new(-143.000000, 145.072601, 1909.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Crafting" = {Position = Vector3.new(-3160.000000, -745.563965, 1684.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Roslit Bay" = {Position = Vector3.new(-1476.000000, 133.500000, 671.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Desolate Deep" = {Position = Vector3.new(-1656.197144, -213.779999, -2846.942383), Rotation = Vector3.new(0.000000, 56.243000, 0.000000)}
-name = "Sunstone" = {Position = Vector3.new(-933.000000, 131.816345, -1119.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Brine Pool" = {Position = Vector3.new(-1794.000000, -142.961349, -3302.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Spike" = {Position = Vector3.new(-1254.000000, 137.389557, 1555.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Statue of Skibidi" = {Position = Vector3.new(73.000000, 141.929993, -1028.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Arch" = {Position = Vector3.new(999.000000, 131.320236, -1237.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Snowcap" = {Position = Vector3.new(2649.000000, 142.283829, 2521.000244), Rotation = Vector3.new(0.000000, -26.242001, 0.000000)}
-name = "Volcano" = {Position = Vector3.new(-1929.942017, 151.588486, 328.903198), Rotation = Vector3.new(0.000000, -3.254000, 0.000000)}
-name = "Vertigo" = {Position = Vector3.new(-110.001328, -515.299377, 1149.999268), Rotation = Vector3.new(180.000000, 2.255000, 180.000000)}
-name = "Mushgrove" = {Position = Vector3.new(2501.000000, 131.000015, -720.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Fischmas 2024" = {Position = Vector3.new(87.360214, 294.499969, -10303.833008), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-name = "Fischmas City" = {Position = Vector3.new(25.335495, 364.635834, -9582.040039), Rotation = Vector3.new(180.000000, 74.999001, 180.000000)}
+    local locations = {
+        ["Moosewood"] = {Position = Vector3.new(470.000000, 150.935791, 260.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["The Depths"] = {Position = Vector3.new(853.240295, -740.365906, 1335.115723), Rotation = Vector3.new(180.000000, 55.258999, 180.000000)},
+        ["Ancient Isles"] = {Position = Vector3.new(5805.021484, 135.301498, 405.922119), Rotation = Vector3.new(0.000000, -42.668999, 0.000000)},
+        ["Forsaken Shores"] = {Position = Vector3.new(-2675.331055, 164.795013, 1758.057129), Rotation = Vector3.new(0.000000, 75.242996, 0.000000)},
+        ["Enchant"] = {Position = Vector3.new(1310.264404, -805.292236, -99.972527), Rotation = Vector3.new(180.000000, -1.252000, 180.000000)},
+        ["Terapin"] = {Position = Vector3.new(-143.000000, 145.072601, 1909.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Crafting"] = {Position = Vector3.new(-3160.000000, -745.563965, 1684.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Roslit Bay"] = {Position = Vector3.new(-1476.000000, 133.500000, 671.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Desolate Deep"] = {Position = Vector3.new(-1656.197144, -213.779999, -2846.942383), Rotation = Vector3.new(0.000000, 56.243000, 0.000000)},
+        ["Sunstone"] = {Position = Vector3.new(-933.000000, 131.816345, -1119.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Brine Pool"] = {Position = Vector3.new(-1794.000000, -142.961349, -3302.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Spike"] = {Position = Vector3.new(-1254.000000, 137.389557, 1555.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Statue of Skibidi"] = {Position = Vector3.new(73.000000, 141.929993, -1028.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Arch"] = {Position = Vector3.new(999.000000, 131.320236, -1237.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Snowcap"] = {Position = Vector3.new(2649.000000, 142.283829, 2521.000244), Rotation = Vector3.new(0.000000, -26.242001, 0.000000)},
+        ["Volcano"] = {Position = Vector3.new(-1929.942017, 151.588486, 328.903198), Rotation = Vector3.new(0.000000, -3.254000, 0.000000)},
+        ["Vertigo"] = {Position = Vector3.new(-110.001328, -515.299377, 1149.999268), Rotation = Vector3.new(180.000000, 2.255000, 180.000000)},
+        ["Mushgrove"] = {Position = Vector3.new(2501.000000, 131.000015, -720.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Fischmas 2024"] = {Position = Vector3.new(87.360214, 294.499969, -10303.833008), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+        ["Fischmas City"] = {Position = Vector3.new(25.335495, 364.635834, -9582.040039), Rotation = Vector3.new(180.000000, 74.999001, 180.000000)}
+    }
+
+
+local interactables = {
+    ["Bait Crate"] = {Position = Vector3.new(383.891144, 136.994125, 333.777832), Rotation = Vector3.new(180.000000, -9.743000, 180.000000)},
+    ["Quality Bait Crate"] = {Position = Vector3.new(-174.103439, 143.167511, 1931.018066), Rotation = Vector3.new(180.000000, 67.754997, 180.000000)},
+    ["Crab Cage"] = {Position = Vector3.new(474.081421, 150.500000, 233.840317), Rotation = Vector3.new(0.000000, -11.755000, 0.000000)},
+    ["GPS"] = {Position = Vector3.new(518.683594, 150.477036, 282.772186), Rotation = Vector3.new(180.000000, -11.407000, 180.000000)},
+    ["Radar"] = {Position = Vector3.new(365.751770, 137.000015, 274.103027), Rotation = Vector3.new(0.000000, 0.001000, 0.000000)},
+    ["Basic Diving gear"] = {Position = Vector3.new(369.762268, 134.456268, 248.701965), Rotation = Vector3.new(0.000000, 0.456000, 0.000000)},
+    ["Advanced Diving gear"] = nil,
+    ["Tidebreaker"] = nil
+}
+
+local totems = {
+    ["Aurora"] = {Position = Vector3.new(-1811.000000, -136.927933, -3282.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Smokescreen"] = {Position = Vector3.new(2789.000000, 139.825485, -625.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Windset"] = {Position = Vector3.new(2849.000000, 178.333237, 2702.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Tempest"] = {Position = Vector3.new(35.000000, 132.500015, 1943.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Sundial"] = {Position = Vector3.new(-1148.000000, 134.499985, -1075.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Eclipse"] = {Position = Vector3.new(5968.000000, 273.752777, 838.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)},
+    ["Meteor"] = {Position = Vector3.new(-1948.000000, 275.356720, 230.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
+}
 
 -- // // // Npcs // // // --
 --idk bro
--- // // // Interactables // // // --
-Bait Crate = {Position = Vector3.new(383.891144, 136.994125, 333.777832), Rotation = Vector3.new(180.000000, -9.743000, 180.000000)}
-Quality Bait Crate = {Position = Vector3.new(-174.103439, 143.167511, 1931.018066), Rotation = Vector3.new(180.000000, 67.754997, 180.000000)}
-Crab Cage = {Position = Vector3.new(474.081421, 150.500000, 233.840317), Rotation = Vector3.new(0.000000, -11.755000, 0.000000)}
-GPS = {Position = Vector3.new(518.683594, 150.477036, 282.772186), Rotation = Vector3.new(180.000000, -11.407000, 180.000000)}
-Radar = {Position = Vector3.new(365.751770, 137.000015, 274.103027), Rotation = Vector3.new(0.000000, 0.001000, 0.000000)}
-Basic Diving gear = {Position = Vector3.new(369.762268, 134.456268, 248.701965), Rotation = Vector3.new(0.000000, 0.456000, 0.000000)}
-Advanced Diving gear = non
-Tidebreaker = non
--- // // // Totems // // // --
-Aurora = {Position = Vector3.new(-1811.000000, -136.927933, -3282.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Smokescreen = {Position = Vector3.new(2789.000000, 139.825485, -625.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Windset = {Position = Vector3.new(2849.000000, 178.333237, 2702.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Tempest = {Position = Vector3.new(35.000000, 132.500015, 1943.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Sundial = {Position = Vector3.new(-1148.000000, 134.499985, -1075.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Eclipse = {Position = Vector3.new(5968.000000, 273.752777, 838.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
-Meteor = {Position = Vector3.new(-1948.000000, 275.356720, 230.000000), Rotation = Vector3.new(0.000000, 0.000000, 0.000000)}
+
 -- // // // Zone Events // // // --
-Isonade
-Whale Shark
-Great White Shark
-Great Hammerhead Shark
-Megalodon
-The Depths - Serpent
+-- Isonade
+-- Whale Shark
+-- Great White Shark
+-- Great Hammerhead Shark
+-- Megalodon
+-- The Depths - Serpent
